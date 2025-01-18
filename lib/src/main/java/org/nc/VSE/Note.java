9f3d0e78b31bf7c3be92db9db50c9a7caee6807e
NBorow <nicholasb0817@gmail.com>
2025-01-18 07:11:52 -0500

    Add category

--- lib/src/main/java/org/nc/VSE/Note.java
+++ lib/src/main/java/org/nc/VSE/Note.java
@@ -1,24 +1,35 @@
 package org.nc.VSE;
 
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 
 public class Note {
     private final Sound instrument;
+    private final SoundCategory category;
     private final int startTick;
     private final int endTick;
     private final float pitch;
     private final float volume;
 
-    public Note(Sound instrument, int startTick, int endTick, float pitch, float volume) {
+    public Note(Sound instrument, SoundCategory category, int startTick, int endTick, float pitch, float volume) {
         this.instrument = instrument;
+        this.category = category;
         this.startTick = startTick;
         this.endTick = endTick;
         this.pitch = pitch;
         this.volume = volume;
     }
 
+    public Note(Sound instrument, int startTick, int endTick, float pitch, float volume) {
+        this(instrument, SoundCategory.MASTER, startTick, endTick, pitch, volume);
+    }
+
     public Sound getInstrument() {
         return instrument;
+    }
+
+    public SoundCategory getCategory() {
+         return category; 
     }
 
     public int getStartTick() {
